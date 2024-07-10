6c21472e515e65ef9c66d9862e7c1ffc4d98b474
remakeaccess22 <alayacyacjude@gmail.com>
2024-07-10 15:49:45 +0800

    Added Simple Service Container

--- controllers/notes/store.php
+++ controllers/notes/store.php
@@ -1,10 +1,10 @@
 <?php
 
 use Core\Database;
+use Core\App;
 use Core\Validator;
 
-$config = require base_path('config.php');
-$db = new Database($config['database']);
+$db = App::resolve(Database::class);
 
 $errors = [];
 
