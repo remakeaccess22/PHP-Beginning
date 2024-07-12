767ad0e5882f4d5d874c090dba7b11c518e4dd09
remakeaccess22 <alayacyacjude@gmail.com>
2024-07-12 15:00:50 +0800

    Password Hashing and Middleware

---
+++ Core/Middleware/Middleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Core\Middleware;
+
+class Middleware {
+
+    const MAP = [
+
+        'auth' => Auth::class,
+        'guest' => Guest::class
+
+    ];
+
+    public static function resolve($key) {
+
+        if(! $key) {
+
+            return;
+        }
+            $middleware = static::MAP[$key] ?? false;
+
+            if(! $middleware) {
+
+                throw new \Exception("Middleware {$key} not found");
+            }
+
+            (new $middleware)->handle();
+    }
+    
+}
